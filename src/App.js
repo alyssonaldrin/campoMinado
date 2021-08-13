d60913347579ff37c2bbf959527a65308dcca778
Alysson 'Inalvejavel' Aldrin <alysson_cdf@hotmail.com>
2021-08-13 17:01:47 -0300

    feat: adiciona index.js
    cria o index.js na pasta raiz, muda o arquivo App.js para pasta src
    (também criada), e muda o main do package.json para o index.js.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+import styles from './styles';
+import Campo from './components/Campo';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>Campo Minado!!!</Text>
+      <Campo />
+    </SafeAreaView>
+  );
+}
